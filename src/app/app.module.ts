3682d24ec1f37338fa4a14bbdc837f8b95fe57b1
Finn <finn.celta@jambit.com>
2019-11-14 10:17:21 +0100

    init bootstrap add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {NavBarComponent} from './nav-bar/nav-bar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MoodOMeterComponent } from './mood-ometer/mood-ometer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,7 +12,6 @@ import {MatButtonModule} from '@angular/material/';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     MoodOMeterComponent,
   ],
   imports: [
--- src/app/mood-ometer/mood-ometer.component.ts
+++ src/app/mood-ometer/mood-ometer.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class MoodOMeterComponent implements OnInit {
 userChoice: string;
 possibilities: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-
+onItemChange(value) {
+  console.log(' Wert: ', value);
+}
   constructor() { }
 
   ngOnInit() {
